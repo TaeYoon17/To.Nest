e8f1f689312cb6431595ed7749690bd1f4b6b255
TaeYoon17 <kim0517@hotmail.com>
2024-01-04 14:44:55 +0900

    Kakao key 설정

--- SolackProject/AppDelegate.swift
+++ SolackProject/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let nativeAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
         return true
     }
 
