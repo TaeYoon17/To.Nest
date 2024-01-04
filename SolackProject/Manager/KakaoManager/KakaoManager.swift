8aad115a92873446a992bdb69684009756bc9c4b
TaeYoon17 <kim0517@hotmail.com>
2024-01-04 15:33:25 +0900

    기본 카카오 로그인 연결

--- SolackProject/Manager/KakaoManager/KakaoManager.swift
+++ SolackProject/Manager/KakaoManager/KakaoManager.swift
@@ -7,8 +7,30 @@
 
 import Foundation
 import RxSwift
-
+import RxKakaoSDKAuth
+import KakaoSDKAuth
+import KakaoSDKUser
+import RxKakaoSDKUser
 final class KakaoManager{
     static let shared = KakaoManager()
-    
+    var disposeBag = DisposeBag()
+    func startLogIn(){
+        print(#function,UserApi.isKakaoTalkLoginAvailable())
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.rx.loginWithKakaoTalk()
+                .subscribe(onNext:{ (oauthToken) in
+                    print("loginWithKakaoTalk() success.")
+                
+                    //do something
+                    let kakaoToken = oauthToken
+                    print("kaakoToken")
+                    print(kakaoToken)
+                }, onError: {error in
+                    print("Error occured!!")
+                    print(error)
+                })
+            .disposed(by: disposeBag)
+        }
+        
+    }
 }
