33811e33de302c6cf12d5b3b4fe6dac0bd1f1e49
TaeYoon17 <46375289+TaeYoon17@users.noreply.github.com>
2025-04-10 23:57:49 +0900

    [Update] Lint 업데이트

--- SolackProject/Services/ToastService/CHToastType.swift
+++ SolackProject/Services/ToastService/CHToastType.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 import UIKit
-enum CHToastType:ToastType{
+enum CHToastType:ToastType {
     case double,complete,isNotChannelAdmin
-    var contents: String{
-        switch self{
+    var contents: String {
+        switch self {
         case .complete: "채널이 생성되었습니다."
         case .double: "워크스페이스에 이미 있는 채널 이름입니다. 다른 이름을 입력해주세요."
         case .isNotChannelAdmin: "채널관리자가 아닙니다."
